d25b29e45920cffd6b0f024b6786f8fd04a630e5
“Jose <josevicente7@gmail.com>
2019-07-06 11:25:03 -0400

    Agrego caso base a la validacion del mutante

--- app/controller/mutant.js
+++ app/controller/mutant.js
@@ -1,4 +1,3 @@
-var operations = require('../db/operations');
 var mutantModel = require('../model/mutant');
 
 var mutantCtrl = {};
@@ -8,28 +7,9 @@ mutantCtrl.isMutant = function (req, res) {
     var isMutant = mutantModel.isMutant(req.body.dna);
 
     if (isMutant) {
-
-        var data = {
-            'dna': req.body.dna,
-            'isMutant': true,
-            'createdAt': new Date()
-        }
-
-        operations.insert('pruebajv', 'mutant', data).then(function (value) {
-            res.status(200).json("200");
-        });
-
+        res.status(200).json("200");
     } else {
-
-        var data = {
-            'dna': req.body.dna,
-            'isMutant': false,
-            'createdAt': new Date()
-        }
-
-        operations.insert('pruebajv', 'mutant', data).then(function (value) {
-            res.status(200).json("403");
-        });
+        res.status(200).json("403");
     }
 };
 
--- app/model/mutant.js
+++ app/model/mutant.js
@@ -1,43 +1,113 @@
+var operations = require('../db/operations');
+
 var mutantModel = {};
 
 mutantModel.isMutant = function (dna) {
 
-    var validChain = 0;
+    /**
+      * DEFINO CASOS BASES
+      * (para evitar realizar operaciones innecesarias)
+    **/
 
-    // Defino casos base (para evitar realizar operaciones innecesarias)
-    
-    // 1ERO Se valida que la matriz sea cuadrada
+    /**
+      * 0
+      * (Se valida que la matriz exista)
+    **/
+    if (dna === null && dna === undefined && dna === 'undefined') {
+        return false;
+    }
+
+    /**
+      * 1
+      * (Se valida que la matriz sea cuadrada NxN)
+    **/
     var x = dna.length;
     var y = dna[0].length;
     console.log('x', x);
     console.log('y', y);
 
-    if(x != y ){
+    if (x != y) {
+        // retorno FALSE, ya que no es una matriz valida de NxN
         return false;
     }
 
-    // 2DO validar que contenga solo las caracteres validos
+    /**
+      * 2
+      * (Se valida que la matriz solo contenga los caracteres validos A, T, C, G )
+    **/
+
+    // Defino una expresion regular que valide si consigue un caracter diferente a A, T, C, G
     var pattInvalidCharacter = /[^ATCG]/g;
 
     var matrix = new Array();
 
     for (var i = 0; i < dna.length; i++) {
+
         var invalid = pattInvalidCharacter.test(dna[i]);
 
-        console.log('Cadena ' + dna[i] + ' invalid ' + invalid);
+        console.log('Cadena: ' + dna[i] + ' Invalida: ' + invalid);
 
-        if(invalid){
+        if (invalid) {
+            // retorno FALSE, ya que se consiguio un caracter diferente a A, T, C, G
             return false;
         }
 
+        // Aprovecho el recorrido para validar los cadenas y genero una matriz real con los valores recibidos
         matrix[i] = dna[i].split('');
+
+        console.log('Matrix', matrix);
     }
 
-    // validar las cadenas horizontales
+    /**
+      * 3
+      * (Se valida si la matriz es menor a 4x4,
+      *  ya que si es menor de 4x4 debe ser un humano ya que no de pueden generar cadenas de 4 caracteres iguales 
+      * seguidos que forman la cadena de un mutante)
+    **/
 
-    // validar las cadenas verticales
+    if (x < 4) {
 
-    console.log('Matrix∫', matrix);
+        var data = {
+            'dna': dna,
+            'isMutant': false,
+            'createdAt': new Date()
+        }
+
+        operations.insert('pruebajv', 'mutant', data).then(function (value) {
+
+        });
+        // retorno FALSE, ya que la matriz mide menos de 4x4, por lo que no se pueden armar cadenas de 4 caracteres
+        return false;
+    }
+
+    // VALIDAR SI EL ADN EXISTE EN LA BD; DE ESA FORMA EVITO EVALUAR UN ADN YA EXISTENTE
+    // Find en la BD con el ADN
+
+    /**
+      * Inicio la evaluación de las cadenas horizontales
+    **/
+    var validChain = 0;
+    // FOR validar con Expresion regular las cadenas AAAA , TTTT, CCCC, GGGG dentro de los String
+    // si consigo 2 casos horizontales ya es exitoso y mando el 200
+
+    var pattvalidadCharacterA = /[^AAAA]/g;
+    var pattvalidadCharacterT = /[^TTTT]/g;
+    var pattvalidadCharacterC = /[^CCCC]/g;
+    var pattvalidadCharacterG = /[^GGGG]/g;
+
+    for (var i = 0; i < dna.length; i++) {
+
+        var validA = pattvalidadCharacterA.test(dna[i]);
+        var validT = pattvalidadCharacterT.test(dna[i]);
+        var validC = pattvalidadCharacterC.test(dna[i]);
+        var validG = pattvalidadCharacterG.test(dna[i]);
+
+        console.log('Cadena: ' + dna[i] + ' Valida A: ' + validA);
+        console.log('Cadena: ' + dna[i] + ' Valida T: ' + validT);
+        console.log('Cadena: ' + dna[i] + ' Valida C: ' + validC);
+        console.log('Cadena: ' + dna[i] + ' Valida G: ' + validG);
+
+    }
 
     return true;
 };
